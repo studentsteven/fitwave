2f62a8cb97b41095d719137b6cfb2f78f87dfc99
FrankBoesveld1 <frankboesveld2006@gmail.com>
2024-09-26 10:29:10 +0200

    heb de header geupdate en heb de footer gemaakt zoals die er uit komt te zien

--- components/navigation/Footer.tsx
+++ components/navigation/Footer.tsx
@@ -1,17 +1,38 @@
-import { StyleSheet, Text, View } from "react-native";
+import { ImageBackground, Platform, StyleSheet, View } from "react-native";
+import NavItem from "./Navitem";
 
 export default function Footer() {
   return (
-    <View style={styles.footer}>
-      <Text onPress={() => alert("test")}>Footer</Text>
+    <View style={{ position: "relative" }}>
+      <ImageBackground
+        resizeMode="cover"
+        source={require("@/assets/backgroundosiris.png")}
+        style={styles.background}
+      />
+
+      <View style={styles.nav}>
+        <NavItem afbeelding="workout" />
+        <NavItem afbeelding="overzicht" />
+        <NavItem afbeelding="home" />
+        <NavItem afbeelding="leaderbord" />
+        <NavItem afbeelding="voeding" />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  footer: {
-    backgroundColor: "#D9D9D9",
-    padding: 12,
+  nav: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    flexDirection: "row",
+    padding: Platform.OS === "ios" ? 10 : 12,
+  },
+  background: {
     width: "100%",
+    height: 160,
+    justifyContent: "center",
+    position: "absolute",
   },
 });
