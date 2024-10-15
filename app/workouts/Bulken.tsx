6b6c5bfc7a79293a22ab685c2f0c4491b9a0e358
Toerna <200356@student.aventus.nl>
2024-10-15 10:36:01 +0200

    Workout afgemaakt

--- app/workouts/Bulken.tsx
+++ app/workouts/Bulken.tsx
@@ -1,206 +1,91 @@
 import Navigation from "@/components/navigation/Navigation";
-import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
+import Workouttabel from "@/components/Workouttabel";
 import "@/global.css";
-import { ScrollView, StyleSheet, Text, View } from "react-native"; // Importeer ScrollView en ImageBackground
+import { ScrollView, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const fullBodyOefeningen = [
+    { naam: "Squats", aantal: "12-15" },
+    { naam: "Push-ups", aantal: "10-12" },
+    { naam: "Bent-over rows (dumbbells)", aantal: "10-12" },
+    { naam: "Lunges", aantal: "12 x 2" },
+    { naam: "Plank (vasthouden)", aantal: "30-60 sec" },
+    { naam: "Deadlifts (dumbbells)", aantal: "10-12" },
+  ];
+  const cardio = [
+    { naam: "Deadlifts", aantal: "6-8" },
+    { naam: "Bench Press", aantal: "6-8" },
+    { naam: "Squats", aantal: "8-10" },
+    { naam: "Barbell Rows", aantal: "8-10" },
+    { naam: "Overhead Press", aantal: "6-8" },
+    { naam: "Pull-Ups", aantal: "8-12" },
+    { naam: "Leg Press", aantal: "8-10" },
+    { naam: "Dumbbell Chest Press", aantal: "8-12" },
+    { naam: "Arnold Press", aantal: "8-12" },
+  ];
   return (
-    <GluestackUIProvider mode="light">
-      <Navigation
-        background="https://tapoutdrinks.com/wp-content/uploads/2021/03/beautiful-fit-people-working-out-in-gym-together-NH4EV43.jpg"
-        title="Bulken"
-      >
-        <View style={styles.container}>
-          {/* Scrollbare tekst */}
-          <ScrollView style={styles.scrollView}>
-            <View style={styles.textBox}>
-              <Text style={styles.text}>
-                <Text style={styles.header}>
-                  Jouw Workout Routine voor Bulken en Spiergroei
-                </Text>
-                {"\n\n"}
-                Klaar om serieus spiermassa op te bouwen en sterker te worden?
-                Deze workout routine is specifiek ontworpen voor spiergroei en
-                bulken. Volg dit plan vier tot vijf dagen per week om maximale
-                spierontwikkeling te bereiken. Vergeet niet om voldoende te eten
-                en te rusten, want spiergroei gebeurt niet alleen in de
-                sportschool, maar ook tijdens het herstel!
-                {"\n\n"}
-                <Text style={styles.subHeader}>Frequentie:</Text> 4-5 dagen per
-                week
-                {"\n"}
-                <Text style={styles.subHeader}>Duur:</Text> 60-90 minuten per
-                sessie
-                {"\n\n\n\n"}
-                <Text style={styles.subHeader}>
-                  Dag 1: Krachttraining (Borst & Triceps)
-                </Text>
-                {"\n\n"}
-                <Text style={styles.subHeader}>Opwarming (5-10 minuten)</Text>
-                {"\n"}
-                Begin met een opwarming zoals licht cardio of dynamisch rekken
-                om je spieren voor te bereiden.
-                {"\n\n"}
-                <Text style={styles.subHeader}>
-                  Krachttraining (4 sets per oefening)
-                </Text>
-                {"\n\n"}
-                {/* Tabel met oefeningen */}
-                <View style={styles.table}>
-                  <Text style={styles.tableHeader}>Oefening</Text>
-                  <Text style={styles.tableHeader}>Herhalingen</Text>
+    <Navigation
+      background="https://img.freepik.com/premium-photo/fitness-trained-muscular-guy-working-out-gym-strong-training-concept-photo_116317-36249.jpg"
+      title="Bulken"
+    >
+      <ScrollView style={styles.scrollView}>
+        <View style={styles.textBox}>
+          <Text style={styles.header}>
+            Jouw Workout Routine voor Bulken
+            {"\n"}
+          </Text>
+          <Text style={styles.tussentext}>
+            Heb je een passie voor krachttraining en het opbouwen van je spiermassa, 
+            maar heb je nog geen idee hoe een routine voor deze doeleinden eruit moet zien? 
+            Dan kunnen wij jou verder helpen met deze voorbeelden van workouts die je spieren laten groeien!
+            {"\n"}
+          </Text>
+          <Text style={styles.subHeader}>Frequentie:</Text>
+          <Text>3-4 dagen per week{"\n"}</Text>
+          <Text style={styles.subHeader}>Duur:</Text>
+          <Text>
+            45-60 minuten per sessie
+            {"\n"}
+          </Text>
+          <Text style={styles.subHeader}>Krachttraining (Full Body)</Text>
+          <Workouttabel oefeningen={fullBodyOefeningen} />
+          <Text style={styles.tussentext}>
+            De onderstaande tabel bevat een groot aantal oefeningen die de spieren testen en de opbouw stimuleren. 
+            Denk eraan om niet jezelf te overbelasten en ook een pauze te nemen om bijvoorbeeld water te consumeren.
+            {"\n"}
+          </Text>
+          <Workouttabel oefeningen={cardio} />
+          <Text style={styles.tussentext}>
+            Voor een optimaal resultaat is de gerecommandeerde frequentie bovenaan de pagina relevant.
+            Wees niet bang om je eigen toevoegingen toe te passen aan de workout! 
+            Dit is eenmaal een algemeene workout voor bulken. 
 
-                  <Text style={styles.tableRow}>Bench Press</Text>
-                  <Text style={styles.tableRow}>15-20</Text>
-                  <Text style={styles.tableRow}>Push-ups</Text>
-                  <Text style={styles.tableRow}>10-12</Text>
-                  <Text style={styles.tableRow}>Incline Dumbbell Press</Text>
-                  <Text style={styles.tableRow}>10-12</Text>
-                  <Text style={styles.tableRow}>Chest Flyes (met kabels)</Text>
-                  <Text style={styles.tableRow}>10-12</Text>
-                  <Text style={styles.tableRow}>Tricep Dips</Text>
-                  <Text style={styles.tableRow}>10-12</Text>
-                  <Text style={styles.tableRow}>Close-grip Bench Press</Text>
-                  <Text style={styles.tableRow}>8-10</Text>
-                </View>
-                {"\n\n"}
-                <Text style={styles.subHeader}>Afkoelen (5-10 minuten)</Text>
-                {"\n"}
-                Sluit af met lichte stretching voor je borst en triceps om de
-                flexibiliteit te verbeteren.
-                {"\n\n\n\n"}
-                <Text style={styles.subHeader}>Dag 2: Rug & Biceps</Text>
-                {"\n\n"}
-                <Text style={styles.subHeader}>Opwarming (5-10 minuten)</Text>
-                {"\n"}
-                Begin met een lichte opwarming, zoals een roeimachine om je rug
-                en biceps op te warmen.
-                {"\n\n"}
-                <Text style={styles.subHeader}>
-                  Krachttraining (4 sets per oefening)
-                </Text>
-                {"\n"}
-                <View style={styles.table}>
-                  <Text style={styles.tableHeader}>Oefening</Text>
-                  <Text style={styles.tableHeader}>Herhalingen</Text>
 
-                  <Text style={styles.tableRow}>Deadlifts</Text>
-                  <Text style={styles.tableRow}>15-20</Text>
-                  <Text style={styles.tableRow}>Push-ups</Text>
-                  <Text style={styles.tableRow}>10-12</Text>
-                  <Text style={styles.tableRow}>Pull-ups</Text>
-                  <Text style={styles.tableRow}>10-12</Text>
-                  <Text style={styles.tableRow}>Barbell Rows</Text>
-                  <Text style={styles.tableRow}>30-35</Text>
-                  <Text style={styles.tableRow}>Dumbbell Curls</Text>
-                  <Text style={styles.tableRow}>10-12</Text>
-                  <Text style={styles.tableRow}>Hammer Curls</Text>
-                  <Text style={styles.tableRow}>10-12</Text>
-                </View>
-                {"\n\n"}
-                <Text style={styles.subHeader}>Afkoelen (5-10 minuten)</Text>
-                Doe een paar minuten lichte stretching voor je rug en biceps.
-                {"\n\n\n\n"}
-                <Text style={styles.subHeader}>
-                  Dag 3: Rust of Actieve Herstel
-                </Text>
-                {"\n"}
-                Neem een rustdag of kies voor een actieve herstelactiviteit
-                zoals lichte cardio, stretching of yoga. Dit helpt je lichaam te
-                herstellen en sterker te worden voor de volgende sessies.
-                {"\n\n\n\n"}
-                <Text style={styles.subHeader}>Dag 4: Benen & Schouders</Text>
-                {"\n\n"}
-                <Text style={styles.subHeader}>Opwarming (5-10 minuten)</Text>
-                {"\n"}
-                Voer een korte warming-up uit, zoals fietsen of lunges, om je
-                benen en schouders voor te bereiden.
-                {"\n\n"}
-                <Text style={styles.subHeader}>
-                  Krachttraining (4 sets per oefening)
-                </Text>
-                {"\n"}
-                <View style={styles.table}>
-                  <Text style={styles.tableHeader}>Oefening</Text>
-                  <Text style={styles.tableHeader}>Herhalingen</Text>
-
-                  <Text style={styles.tableRow}>Squats</Text>
-                  <Text style={styles.tableRow}>8-10</Text>
-                  <Text style={styles.tableRow}>Lunges</Text>
-                  <Text style={styles.tableRow}>10-12 per been</Text>
-                  <Text style={styles.tableRow}>Leg Press</Text>
-                  <Text style={styles.tableRow}>8-10</Text>
-                  <Text style={styles.tableRow}>Shoulder Press</Text>
-                  <Text style={styles.tableRow}>8-10</Text>
-                  <Text style={styles.tableRow}>Lateral Raises</Text>
-                  <Text style={styles.tableRow}>10-12</Text>
-                </View>
-                {"\n\n"}
-                <Text style={styles.subHeader}>Afkoelen (5-10 minuten)</Text>
-                {"\n\n"}
-                Zorg voor voldoende cooling-down met stretching voor je benen en
-                schouders.
-                {"\n\n"}
-              </Text>
-            </View>
-          </ScrollView>
+          </Text>
         </View>
-      </Navigation>
-    </GluestackUIProvider>
+      </ScrollView>
+    </Navigation>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    display: "flex",
-    justifyContent: "flex-start",
-    alignItems: "center",
-    backgroundColor: "#f5f5f5",
-  },
   scrollView: {
     width: "100%",
   },
   textBox: {
     backgroundColor: "#FFFFFF",
-    borderRadius: 10, // Ronde hoeken
-    padding: 20, // Binnenruimte
-    elevation: 3, // Schaduw voor Android
-    shadowColor: "#000", // Schaduwkleur voor iOS
-    shadowOffset: { width: 0, height: 2 }, // Schaduw offset
-    shadowOpacity: 0.3, // Schaduw transparantie
-    shadowRadius: 4, // Schaduw radius
-  },
-  text: {
-    fontSize: 16, // Lettergrootte
-    color: "#000", // Tekstkleur
-    textAlign: "left", // Links uitgelijnde tekst
+    padding: 15,
   },
   header: {
     fontSize: 24,
     fontWeight: "bold",
-    marginBottom: 10,
   },
   subHeader: {
     fontSize: 16,
     fontWeight: "600",
-    marginBottom: 5,
   },
-  tableWrapper: {
-    marginBottom: 10, // Tussenruimte onder de tabel
-    width: "100%",
-    alignItems: "center", // Gecentreerd
-    justifyContent: "center",
+  tussentext: {
+    fontSize: 15
   },
-  tableHeader: {
-    fontWeight: "bold",
-    fontSize: 16,
-  },
-  tableRow: {
-    fontSize: 16,
-    alignContent: "space-between",
-  },
-  table: {
-    marginBottom: 10, // Tussenruimte onder de tabel
-  },
+  
 });
--- app/workouts/Gezondheid.tsx
+++ app/workouts/Gezondheid.tsx
@@ -1,166 +1,79 @@
 import Navigation from "@/components/navigation/Navigation";
-import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
+import Workouttabel from "@/components/Workouttabel";
 import "@/global.css";
-import { ScrollView, StyleSheet, Text, View } from "react-native"; // Importeer ScrollView en ImageBackground
+import { ScrollView, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const fullBodyOefeningen = [
+    { naam: "Rennen/Wandelen", aantal: "10-15 min" },
+    { naam: "Intervaltraining", aantal: "1-2 min" },
+    { naam: "Squats", aantal: "10-15" },
+    { naam: "Push-ups", aantal: "10-15" },
+    { naam: "Lunges", aantal: "10-12 x 2" },
+    { naam: "Plank", aantal: "30-60 sec" },
+    { naam: "Hamstring Stretch", aantal: "30 sec x 2" },
+    { naam: "Cat-Cow Stretch", aantal: "1-2 min" },
+    { naam: "Child's Pose", aantal: "30 sec" },
+    { naam: "Ademhalingsoefeningen", aantal: "1-2 min" }
+  ];
+  
+ 
   return (
-    <GluestackUIProvider mode="light">
-      <Navigation
-        background="https://tapoutdrinks.com/wp-content/uploads/2021/03/beautiful-fit-people-working-out-in-gym-together-NH4EV43.jpg"
-        title="Gezondheid"
-      >
-        <View style={styles.container}>
-          {/* Scrollbare tekst */}
-          <ScrollView style={styles.scrollView}>
-            <View style={styles.textBox}>
-              <Text style={styles.text}>
-                <Text style={styles.header}>
-                  Jouw Workout Routine voor Betere Gezondheid
-                </Text>
-                {"\n\n"}
-                Deze routine is ontworpen om je algemene gezondheid en welzijn
-                te verbeteren. Het bevat een mix van krachttraining,
-                cardiovasculaire oefeningen, flexibiliteit en herstel. Volg deze
-                routine drie tot vier keer per week om je fitheid te verbeteren,
-                je energie te verhogen en je gezondheid te ondersteunen!
-                {"\n\n"}
-                <Text style={styles.subHeader}>Frequentie:</Text> 3-4 dagen per
-                week
-                {"\n"}
-                <Text style={styles.subHeader}>Duur:</Text> 45-60 minuten per
-                sessie
-                {"\n\n"}
-                <Text style={styles.subHeader}>Dag 1: Full Body Workout</Text>
-                {"\n\n"}
-                <Text style={styles.subHeader}>Opwarming (5-10 minuten)</Text>
-                {"\n"}
-                Begin met een lichte cardiovasculaire opwarming zoals fietsen,
-                wandelen of touwtjespringen.
-                {"\n\n"}
-                <Text style={styles.subHeader}>
-                  Krachttraining en Cardiovasculair (3 sets)
-                </Text>
-                {"\n\n"}
-                {/* Tabel voor Full Body workout */}
-                <View style={styles.table}>
-                  <Text style={styles.tableHeader}>Oefening</Text>
-                  <Text style={styles.tableHeader}>Herhalingen</Text>
-
-                  <Text style={styles.tableRow}>Squats</Text>
-                  <Text style={styles.tableRow}>15-20</Text>
-                  <Text style={styles.tableRow}>Push-ups</Text>
-                  <Text style={styles.tableRow}>10-12</Text>
-                  <Text style={styles.tableRow}>Lunges</Text>
-                  <Text style={styles.tableRow}>12 per been</Text>
-                  <Text style={styles.tableRow}>Plank (vasthouden)</Text>
-                  <Text style={styles.tableRow}>30-60 sec</Text>
-                  <Text style={styles.tableRow}>
-                    Deadlifts (met lichaamsgewicht of lichte gewichten)
-                  </Text>
-                  <Text style={styles.tableRow}>10-12</Text>
-                </View>
-                {"\n"}
-                <Text style={styles.subHeader}>Afkoelen (5-10 minuten)</Text>
-                {"\n"}
-                Stretch je buikspieren en onderrug.
-                {"\n\n"}
-                <Text style={styles.subHeader}>
-                  Dag 2: Flexibiliteit en Mobiliteit
-                </Text>
-                {"\n"}
-                <Text style={styles.subHeader}>Opwarming (5-10 minuten)</Text>
-                {"\n"}
-                Begin met lichte dynamische stretches zoals armcirkels en
-                beenzwaaien.
-                {"\n\n"}
-                <Text style={styles.subHeader}>
-                  Yoga en Stretching (3 sets)
-                </Text>
-                {"\n\n"}
-                {/* Tabel voor Flexibiliteit en Mobiliteit */}
-                <View style={styles.table}>
-                  <Text style={styles.tableHeader}>Oefening</Text>
-                  <Text style={styles.tableHeader}>Herhalingen</Text>
-
-                  <Text style={styles.tableRow}>Plank</Text>
-                  <Text style={styles.tableRow}>30-60 sec</Text>
-                  <Text style={styles.tableRow}>Push-ups</Text>
-                  <Text style={styles.tableRow}>10-12</Text>
-                  <Text style={styles.tableRow}>Russian Twists</Text>
-                  <Text style={styles.tableRow}>15 per kant</Text>
-                  <Text style={styles.tableRow}>Mountain Climbers</Text>
-                  <Text style={styles.tableRow}>30 sec</Text>
-                  <Text style={styles.tableRow}>Leg Raises</Text>
-                  <Text style={styles.tableRow}>10-12</Text>
-                </View>
-                {"\n\n"}
-                <Text style={styles.subHeader}>Afkoelen (5-10 minuten)</Text>
-                {"\n"}
-                Doe langzame ademhalingsoefeningen en volledige
-                lichaamstretching.
-                {"\n"}
-              </Text>
-            </View>
-          </ScrollView>
+    <Navigation
+      background="https://gezondnu.nl/content/uploads/2021/10/202015.gezondnu-bewegen-maakt-gelukkig-scaled-1.jpg"
+      title="Gezondheid"
+    >
+      <ScrollView style={styles.scrollView}>
+        <View style={styles.textBox}>
+          <Text style={styles.header}>
+            Jouw Workout Routine voor een gezonde levenstijl
+            {"\n"}
+          </Text>
+          <Text style={styles.tussentext}>
+            Als je behoefde hebt aan een gezonde leefstijl, 
+            dan is deze workout voor jou! 
+            Met deze oefeningen houd je je lichaam flexibel en mobiel.
+            {"\n"}
+          </Text>
+          <Text style={styles.subHeader}>Frequentie:</Text>
+          <Text>willekeurig{"\n"}</Text>
+          <Text style={styles.subHeader}>Duur:</Text>
+          <Text>
+            willekeurig
+            {"\n"}
+          </Text>
+          <Text style={styles.subHeader}>Algemene gezonde oefeningen</Text>
+          <Workouttabel oefeningen={fullBodyOefeningen} />
+          <Text style={styles.tussentext}>
+          Door consequent te investeren in je gezondheid, 
+          verbeter je niet alleen je fysieke fitheid, 
+          maar ook je mentale welzijn. 
+          Vergeet niet dat herstel net zo belangrijk is als inspanning
+            {"\n"}
+          </Text>
         </View>
-      </Navigation>
-    </GluestackUIProvider>
+      </ScrollView>
+    </Navigation>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    display: "flex",
-    justifyContent: "flex-start",
-    alignItems: "center",
-    backgroundColor: "#f5f5f5", // Achtergrondkleur voor contrast
-  },
   scrollView: {
-    width: "100%", // Zorg dat de ScrollView de volledige breedte heeft
+    width: "100%",
   },
   textBox: {
-    backgroundColor: "#FFFFFF", // Achtergrondkleur van het tekstvlak
-    borderRadius: 10, // Ronde hoeken
-    padding: 20, // Binnenruimte
-    elevation: 3, // Schaduw voor Android
-    shadowColor: "#000", // Schaduwkleur voor iOS
-    shadowOffset: { width: 0, height: 2 }, // Schaduw offset
-    shadowOpacity: 0.3, // Schaduw transparantie
-    shadowRadius: 4, // Schaduw radius
-  },
-  text: {
-    fontSize: 16, // Lettergrootte
-    color: "#000", // Tekstkleur
-    textAlign: "left", // Links uitgelijnde tekst
+    backgroundColor: "#FFFFFF",
+    padding: 15,
   },
   header: {
     fontSize: 24,
     fontWeight: "bold",
-    marginBottom: 10,
   },
   subHeader: {
     fontSize: 16,
     fontWeight: "600",
-    marginBottom: 5,
   },
-  tableWrapper: {
-    marginBottom: 10, // Tussenruimte onder de tabel
-    width: "100%",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-
-  table: {
-    marginBottom: 10, // Tussenruimte onder de tabel
-  },
-  tableHeader: {
-    fontWeight: "bold",
-    fontSize: 16,
-  },
-  tableRow: {
-    fontSize: 16,
-    alignContent: "space-between",
+  tussentext: {
+    fontSize: 15
   },
 });
